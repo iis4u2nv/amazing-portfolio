802842837de09f655d18070a09a397682746ed2c
amazing grace <agracenotary@gmail.com>
2023-09-11 22:23:32 -0400

    finally added the correct footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,15 @@
-
-function Footer () {
+function Footer() {
   const style = {
-    display:'none' 
-  }
-    return(
-        <div>
-          <a href='https://github.com/iis4u2nv'>GitHub</a>
-          <br></br>
-          <a href='https://stackoverflow.com/'>Stackoverflow</a>
-          <br></br>
-          <a href='https://twitter.com/'>Twitter</a>
-
-        </div>
-    )
+    display: "none",
+  };
+  return (
+    <div>
+      <a href="https://github.com/iis4u2nv">GitHub</a>
+      <br></br>
+      <a href="https://stackoverflow.com/">Stackoverflow</a>
+      <br></br>
+      <a href="https://twitter.com/">Twitter</a>
+    </div>
+  );
 }
 export default Footer;
